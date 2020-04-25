af8751a011274a273d3cc3b8f12a3e0bdc82bf0e
Tyler Stendara <tylerstendara@Tylers-MacBook-Pro-2.local>
2020-04-25 01:11:22 -0500

    added simple tests for file creation and deletion

--- testing.py
+++ testing.py
@@ -6,26 +6,13 @@ import subprocess
 class testingMethods(unittest.TestCase):
         
     def test_creatingEmailFile(self):
-        helper.email("s", "s", "s", "s")
+        helper.email("", "", "", "")
         self.assertTrue(helper.findFile())
 
     def test_creatingTextFile(self):
         helper.text("", "", "", "", "")
         self.assertTrue(helper.findFile())
 
-    def test_runningWatchingQueue(self):
-        res = os.system("python watchingQueue.py")
-        print('oi')
-        boolean = ("..Training bot will let you know when you're in a game!" == res)
-        self.assertTrue(res)
-
-    # def test_deletion(self):
-    #     os.system("del credentials.txt")
-    #     self.assertFalse(helper.findFile())
-
-
-        
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_deletion(self):
+        os.system("del credentials.txt")
+        self.assertFalse(helper.findFile())
