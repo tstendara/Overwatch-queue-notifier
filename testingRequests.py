f857f6476c7450a6ab12f7fc4019622acf1a7967
tyler stendara <tyler.stendara@ibm.com>
2020-04-27 21:45:05 -0500

    removing files

--- testingRequests.py
+++
@@ -1,9 +0,0 @@
-import helper
-import random 
-
-def test():
-    for x in range(100):
-        queueTime = random.randint(0, 1000)
-        helper.sendingEmail({"email":"test@email.com", "queueTime":""})
-
-test()
