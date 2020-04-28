cfdf98b8e018635ade97b62033e0b18980c28d3c
tyler stendara <tyler.stendara@ibm.com>
2020-04-27 20:16:12 -0500

    cleaning

--- testingRequests.py
+++ testingRequests.py
@@ -4,6 +4,6 @@ import random
 def test():
     for x in range(100):
         queueTime = random.randint(0, 1000)
-        helper.sendingEmail({"email":"pixelsedgegaming@gmail.com", "queueTime":""})
+        helper.sendingEmail({"email":"test@email.com", "queueTime":""})
 
 test()
